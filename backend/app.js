e7ea6ee24ab8c0bc37dc0c883dc2e8fc3cdf0659
Dylan Hague <haguedylan@gmail.com>
2021-02-24 15:14:47 -0500

    Feat: Fetch Posts from Backend

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use('/api/posts', (req,res,next) => {
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, OPTIONS")
+  next();
+});
+
+app.use('/api/posts', (req, res, next) => {
   const posts = [
-    { id: 'jask2jk2ls9', title: 'First server-side post', content: "This is coming from the server"},
-    { id: '23k4l5g9axj', title: 'Second server-side post', content: "This is coming from the server!"},
+    { id: 'jask2jk2ls9', title: 'First server-side post', content: "This is coming from the server" },
+    { id: '23k4l5g9axj', title: 'Second server-side post', content: "This is coming from the server!" },
   ];
   res.status(200).json({
     message: 'Posts Fetched Succesfully!',
