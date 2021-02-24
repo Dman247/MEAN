7dbb2dbf17a01b3b008b654d494892dda34737b3
Dylan Hague <haguedylan@gmail.com>
2021-02-24 14:28:23 -0500

    Serve Hard-Coded Posts; /api/posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-  console.log('first middleware');
-  next();
-});
-
-app.use((req,res,next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req,res,next) => {
+  const posts = [
+    { id: 'jask2jk2ls9', title: 'First server-side post', content: "This is coming from the server"},
+    { id: '23k4l5g9axj', title: 'Second server-side post', content: "This is coming from the server!"},
+  ];
+  res.status(200).json({
+    message: 'Posts Fetched Succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
