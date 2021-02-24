2164396bbb4136e4708935310348080c871494c0
Dylan Hague <haguedylan@gmail.com>
2021-02-24 18:08:35 -0500

    Feat: Add Post
    Send to Backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,7 +14,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added sucessfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     { id: 'jask2jk2ls9', title: 'First server-side post', content: "This is coming from the server" },
     { id: '23k4l5g9axj', title: 'Second server-side post', content: "This is coming from the server!" },
