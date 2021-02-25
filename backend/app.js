b68f4f9a945cf805f9a715f9c9566543f1f0bd3a
Dylan Hague <haguedylan@gmail.com>
2021-02-25 12:17:48 -0500

    Backend: Save posts to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 
+const Post = require('./models/post');
 const dbConfig = require('../config/db.js');
 
 const app = express();
@@ -26,8 +27,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added sucessfully'
   });
@@ -38,10 +42,10 @@ app.get('/api/posts', (req, res, next) => {
     { id: 'jask2jk2ls9', title: 'First server-side post', content: "This is coming from the server" },
     { id: '23k4l5g9axj', title: 'Second server-side post', content: "This is coming from the server!" },
   ];
-  res.status(200).json({
-    message: 'Posts Fetched Succesfully!',
+      res.status(200).json({
+        message: 'Posts Fetched Succesfully!',
     posts: posts
-  });
+      });
 });
 
 module.exports = app;
