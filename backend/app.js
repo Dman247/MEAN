a7ece764fbdc1590eb8bc31bfaed787c6e3d9974
Dylan Hague <haguedylan@gmail.com>
2021-02-25 12:19:15 -0500

    Feat: getPosts from MongoDB

--- backend/app.js
+++ backend/app.js
@@ -38,14 +38,13 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: 'jask2jk2ls9', title: 'First server-side post', content: "This is coming from the server" },
-    { id: '23k4l5g9axj', title: 'Second server-side post', content: "This is coming from the server!" },
-  ];
+  Post.find()
+    .then(documents => {
       res.status(200).json({
         message: 'Posts Fetched Succesfully!',
-    posts: posts
+        posts: documents
       });
+    });
 });
 
 module.exports = app;
