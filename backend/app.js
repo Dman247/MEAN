740133b2257fa0c2496f4dfce36b28d2099e121a
Dylan Hague <haguedylan@gmail.com>
2021-02-25 12:15:20 -0500

    Connect Backend to MongoDB
    Connect Backend to MongoDB
    
    Fix

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
+const dbConfig = require('../config/db.js');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://dylan:" + dbConfig.password + "@mean-course.o3p4v.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection Failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
