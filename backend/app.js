3dea881985a39b89b18fa2e284c1e60dd9346b1a
Dylan Hague <haguedylan@gmail.com>
2021-02-26 15:22:38 -0500

    BugFix: Cannot Delete a Post Immediately After Creation

--- backend/app.js
+++ backend/app.js
@@ -31,9 +31,11 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added sucessfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added sucessfully',
+      postId: createdPost._id,
+    });
   });
 });
 
