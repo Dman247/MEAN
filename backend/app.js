ab84d9e5cca1255c0eabed9d7971b54ee06e3fbf
Dylan Hague <haguedylan@gmail.com>
2021-03-11 11:28:57 -0500

    Display the Post's Image On the Post-List

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
@@ -17,7 +18,7 @@ mongoose.connect("mongodb+srv://dylan:" + dbConfig.password + "@mean-course.o3p4
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use("/images", express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
