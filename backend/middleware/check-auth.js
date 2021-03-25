7e362876e76a0689356416109d2c1b3195b8566e
Dylan Hague <haguedylan@gmail.com>
2021-03-25 08:56:23 -0400

    Backend-Feat: Check-Auth Middleware

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, "secret_key_to_encrypt_JWT")
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Auth failed!' });
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
