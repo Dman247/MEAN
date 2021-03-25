dc29597c09b013aa4a081cd2db3d2d14a9322501
Dylan Hague <haguedylan@gmail.com>
2021-03-25 17:24:41 -0400

    UX: Redirect on Login/Signup/Logout -- With Spinners

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
@@ -10,7 +11,7 @@ export class AuthService {
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
 
   }
 
@@ -31,7 +32,7 @@ export class AuthService {
 
     this.http.post('http://localhost:3030/api/user/signup', authData)
       .subscribe(response => {
-        console.log(response);
+        this.router.navigate(['/', 'login']);
       });
   }
 
@@ -44,6 +45,7 @@ export class AuthService {
         if (token) {
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          this.router.navigate(['/']);
         }
       });
   }
@@ -52,6 +54,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 
 }
