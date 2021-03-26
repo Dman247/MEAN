e45d62ae94f7d283aff8c8173c35593357d16a49
Dylan Hague <haguedylan@gmail.com>
2021-03-26 14:46:53 -0400

    UI: Call Logout() at Token Expiration

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -54,7 +54,8 @@ router.post('/login', (req, res, next) => {
       }
       const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id }, 'secret_key_to_encrypt_JWT', { expiresIn: '1h' });
       res.status(200).json({
-        token
+        token,
+        expiresIn: '3600',
       });
     })
     .catch(err => {
