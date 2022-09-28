921d83a74225dd65b449344341a051e24410d19c
Dylan Hague <haguedylan@gmail.com>
2022-09-27 23:24:40 -0400

    Change Backend URI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://meanproject-env.eba-vkp7k9c9.us-east-2.elasticbeanstalk.com/api'
+  apiUrl: 'https://mean.api.dylanhague.com/api'
 };
